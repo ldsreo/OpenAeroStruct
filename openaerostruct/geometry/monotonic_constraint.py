59f6f2531c459a5fb47fd84e0cea7edce836c4e4
Leonardo S. R. Oliveira <ldsreo@pm.me>
2022-07-07 14:41:56 +0100

    generalize class for input of arbitrary size

--- openaerostruct/geometry/monotonic_constraint.py
+++ openaerostruct/geometry/monotonic_constraint.py
@@ -23,6 +23,7 @@ class MonotonicConstraint(om.ExplicitComponent):
     def initialize(self):
         self.options.declare("var_name", types=str)
         self.options.declare("surface", types=dict)
+        self.options.declare("size", types=int)
 
     def setup(self):
 
@@ -31,26 +32,28 @@ class MonotonicConstraint(om.ExplicitComponent):
         self.con_name = "monotonic_" + self.var_name
 
         self.symmetry = surface["symmetry"]
-        self.ny = surface["mesh"].shape[1]
 
-        self.add_input(self.var_name, val=np.zeros(self.ny))
-        self.add_output(self.con_name, val=np.zeros(self.ny - 1))
+        size_in = self.options["size"]
+        size_out = size_in-1
 
-        rows = np.arange(0, self.ny - 1)
+        self.add_input(self.var_name, val=np.zeros(size_in))
+        self.add_output(self.con_name, val=np.zeros(size_out))
+
+        rows = np.arange(0, size_out)
         rows = np.vstack((rows, rows)).flatten(order="F")
 
-        cols = np.arange(1, self.ny - 1)
+        cols = np.arange(1, size_out)
         cols = np.vstack((cols, cols)).flatten(order="F")
         cols = np.insert(cols, 0, 0)
-        cols = np.append(cols, self.ny - 1)
+        cols = np.append(cols, size_out)
 
         sparse_val = np.ones_like(rows)
         sparse_val[1::2] = -1
         if not self.symmetry:
-            if self.ny % 2 == 0:
-                sparse_val[self.ny - 2 :] *= -1
+            if size_in % 2 == 0:
+                sparse_val[size_out - 1 :] *= -1
             else:
-                sparse_val[self.ny - 1 :] *= -1
+                sparse_val[size_out :] *= -1
 
         self.declare_partials(self.con_name, self.var_name, rows=rows, cols=cols, val=sparse_val)
 
